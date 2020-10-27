aae837e5bd8ef48dcd5f4736c6fad8e85609324c
Shebanov Nikita <iloveparka@gmail.com>
2020-10-27 16:18:18 +0300

    First commit

---
+++ Practice/02/C++/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a = 3;
+    int b = 3.14;
+    double z = 3;
+    double v = 3.14;
+    cout << a << endl << b << endl << z << endl << v;
+     
+}
---
+++ Practice/03/C++/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int nail;
+    int bain;
+    cin >> nail >> bain;
+    if (bain != 0) {
+        cout << nail + bain << endl << nail - bain << endl << nail * bain << endl << nail / bain << endl;
+    }
+    else cout << nail + bain << endl << nail - bain << endl << nail * bain << endl << "Делить на 0 нельзя" << endl;
+    double nail2;
+    double bain2;
+    cin >> nail2 >> bain2;
+    if (bain2 != 0) {
+        cout << nail2 + bain2 << endl << nail2 - bain2 << endl << nail2 * bain2 << endl << nail2 / bain2 << endl;
+    }
+    else cout << nail2 + bain2 << endl << nail2 - bain2 << endl << nail2 * bain2 << endl << "Делить на 0 нельзя" << endl;
+
+}
