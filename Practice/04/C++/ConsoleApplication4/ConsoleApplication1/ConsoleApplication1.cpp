a46c166ab3306034c4bd6f1563f786b85123dcde
Shebanov Nikita <iloveparka@gmail.com>
2020-10-27 16:37:27 +0300

    add 04

---
+++ Practice/04/C++/ConsoleApplication4/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    cin >> a >> b;
+    c = a;
+    a = b;
+    b = c;
+    cout << a << endl << b << endl;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << a << endl << b << endl;
+
+}
